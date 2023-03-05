1bec29881fb4e78dc0715d178bc22b6d363123a4
OM-ITM <120074119+OM-ITM@users.noreply.github.com>
2023-03-06 00:45:43 +0530

    Update print even number upto n.py

--- print even number upto n.py
+++ print even number upto n.py
@@ -1,9 +1,22 @@
 #even number upto n
 x=int(input("enter any number\n"))
 y=1
-if x==0:
+if x==0 or x==1:
 	print("invalid input")
 while y<=x:
 	if y%2==0:
 		print(y)
 	y=y+1
+# another way
+x=int(input("enter any number greayer than one:\t"))
+for i in range(2,x+1,2):
+	print(i)
+	
+# another one
+x=int(input("enter any number\n"))
+y=2
+if x==0 or x==1:
+	print("invalid input")
+while y<=x:
+	print(y)#if y%2==0:
+	y=y+2
